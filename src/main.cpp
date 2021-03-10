9e7716e78a23b4c95589b26a4ab24fb2acac222d
nqriver <nqriver@gmail.com>
2021-03-11 00:44:25 +0100

    added test function for operator and container utilities

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,42 @@
 #include <algorithm>
 #include "../includes/IntArray.h"
 
+void test();
+
 int main() {
-    IntArray arr(5);
-    for (int i { 0 }; i < 5; ++i){
-        arr[i] = 10 - i;
+    test();
+    return 0;
+}
+
+void test(){
+    constexpr int size { 50 };
+    IntArray arr(size);
+    for (int i { 0 }; i < size; ++i){
+        arr[i] = size - i;
     }
+    std::cout << arr;
+
     auto iter = arr.begin();
     while (iter != arr.end()){
-        std::cout << ++(*(iter++)) << " ";
+        ++(*(iter++));
     }
-    arr.print();
-    return 0;
+    std::cout << arr;
+
+    for (auto &element : arr){
+        element += 100;
+    }
+
+    arr.resize(70);
+
+    std::cout << arr;
+    std::sort(arr.begin(), arr.end());
+    std::cout << arr;
+    int indexToRemove {arr.size()-1};
+    arr.remove(indexToRemove);
+    arr[0] = 22;
+    arr.insertBefore(899, 6);
+    std::cout << arr;
+    std::fill(arr.begin(), arr.end(), 2);
+    std::cout << arr;
+    arr.erase();
 }
